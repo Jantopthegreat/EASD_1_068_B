9e4b0fb648cf366ab9c7d68e9e4d0ff21af6f14a
Jantopthegreat <fauzangriya@gmail.com>
2023-04-07 10:17:45 +0700

    memasukan variable sesuai dengan perintah yang dimana arr[] diubah ke namapanggilan[] dan "j" dirubah ke nama inisial

--- EASD_1_068_B/EASD_1_068_B.cpp
+++ EASD_1_068_B/EASD_1_068_B.cpp
@@ -2,3 +2,6 @@
 using namespace std;
 
 const int MAX_SIZE = 88; // 68 + 20 - 2 * 15 + 10 + 20 = 88 (68 = 2 digit nim belakang saya) 
+void merge(int ojan[], int low, int mid, int high) { // ARR DIGANTI OLEH NICKNAME / NAMA SEBUTAN, NAMA SEBUTAN SAYA OJAN
+    int i = low, FR = mid + 1, k = low; //NAMA SAYA : FAUZAN ALTHAF RIANTO, MAKA INISIAL SAYA FR
+
