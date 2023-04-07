b2de9ccac7551bf0b7588106a7d066c5bae756a8
Jantopthegreat <fauzangriya@gmail.com>
2023-04-07 10:32:59 +0700

    kemungkinan ke2 jika nilai tengah lebih kecil sama dengan nilai tertinggi

--- EASD_1_068_B/EASD_1_068_B.cpp
+++ EASD_1_068_B/EASD_1_068_B.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 using namespace std;
 
-const int MAX_SIZE = 88; // 68 + 20 - 2 * 15 + 10 + 20 = 88 (68 = 2 digit nim belakang saya) 
+const int MAX_SIZE = 88; // 68 + 20 - 2 * 15 + 10 + 20 = 88 (68 = 2 digit nim belakang saya)
+
 void merge(int ojan[], int low, int mid, int high) { // ARR DIGANTI OLEH NICKNAME / NAMA SEBUTAN, NAMA SEBUTAN SAYA OJAN
-    int i = low, FR = mid + 1, k = low; //NAMA SAYA : FAUZAN ALTHAF RIANTO, MAKA INISIAL SAYA FR 
+    int i = low, FR = mid + 1, k = low; //NAMA SAYA : FAUZAN ALTHAF RIANTO, MAKA INISIAL SAYA FR
     int b[MAX_SIZE];
     while (i <= mid && FR <= high) {
         if (ojan[i] <= ojan[FR]) {
             b[k] = ojan[i];
             i++;
         }
+        else {
+            b[k] = ojan[FR];
+            FR++;
+        }
+        k++;
+    }
+    while (FR <= high) {
+        b[k] = ojan[FR];
+        FR++;
+        k++;
+    }
+
+   
+
 
 
